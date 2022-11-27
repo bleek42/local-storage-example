a8c9fbe69c31ead2cea0546f224804da2eddf11b
bleek42 <brandonleek42@gmail.com>
2022-11-27 16:01:57 -0500

    finshed example excercise with student

---
+++ index.js
@@ -0,0 +1,62 @@
+function getLocalStorage() {
+  console.info('getting information from local stroage...')
+  const data = localStorage.getItem('data'); // JSON object that JS cannot read on its own
+  console.log(typeof data);
+  const parsedData = JSON.parse(data); // object that JS can read
+  console.log(parsedData);
+  return parsedData;
+}
+
+function setLocalStorage(data = []) {
+  console.info('setting information to local stroage...');
+  const hasData = getLocalStorage();
+  if (hasData) {
+    console.log('I have data in local storage!', hasData);
+    for (let i = 0; 1 < hasData.data.length; i++) {
+      const element = array[i];
+      if(element.id === data[i].id) {
+         console.error('')
+      }
+    }
+  }
+  // localStorage.setItem('data', data);
+  // console.log(save);
+  
+};
+
+const dataContainer = document.querySelector('#local-storage-container');
+const formElem = document.querySelector('#search');
+const clearBtn = document.querySelector('#clear');
+
+function displayData(data) {
+  const dataToDisplay = {
+			"id": 1,
+			data
+  }
+  const dataList = document.createElement('ul');
+  dataList.textContent = dataToDisplay.data;
+  dataContainer.append(dataList)
+
+}
+
+formElem.addEventListener('submit', function(event) {
+  event.preventDefault()
+  console.log('submitting form...', event.target.children[1].value);
+  displayData(event.target.children[1].value);
+})
+
+// const myData = JSON.stringify({
+// 	"data": [
+// 		{
+// 			"id": 2,
+// 			"data": "my saved data"
+// 		}
+// 	]
+// });
+
+// const myData = {
+//   id: 2,
+//   content: "this is my other data"
+// }
+
+// setLocalStorage(JSON.stringify(myData));
