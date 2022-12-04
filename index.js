e082cce1204357734532401ad1097bf03ad8c721
bleek42 <brandonleek42@gmail.com>
2022-12-04 12:13:02 -0500

    added footer

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function setLocalStorage(data = []) {
     for (let i = 0; 1 < hasData.data.length; i++) {
       const element = array[i];
       if(element.id === data[i].id) {
-         console.error('')
+         console.error('');
       }
     }
   }
@@ -29,34 +29,43 @@ const formElem = document.querySelector('#search');
 const clearBtn = document.querySelector('#clear');
 
 function displayData(data) {
+
   const dataToDisplay = {
-			"id": 1,
-			data
+		"id": 1,
+		data
   }
+
   const dataList = document.createElement('ul');
   dataList.textContent = dataToDisplay.data;
-  dataContainer.append(dataList)
-
+  dataContainer.append(dataList);
 }
+
+const jsonData = JSON.stringify({
+	"data": [
+		{
+			"id": 2,
+			"data": "my saved data"
+		}
+	]
+});
+
+const objData = {
+  id: 2,
+  content: "this is my other data"
+}
+
+function clearData() {
+  for (let i = 0; i < dataContainer.children.length; i++) {
+    const element = array[i];
+    console.log(element);
+    element.textContent = ''
+  }
+}
+
+clearBtn.onclick
 
 formElem.addEventListener('submit', function(event) {
   event.preventDefault()
   console.log('submitting form...', event.target.children[1].value);
   displayData(event.target.children[1].value);
 })
-
-// const myData = JSON.stringify({
-// 	"data": [
-// 		{
-// 			"id": 2,
-// 			"data": "my saved data"
-// 		}
-// 	]
-// });
-
-// const myData = {
-//   id: 2,
-//   content: "this is my other data"
-// }
-
-// setLocalStorage(JSON.stringify(myData));
